a33ab64e69f5ffbf76fe5bb7537b56c5f98bc6eb
Rafael Kerr <rafael.kerr@gmail.com>
2014-10-03 23:44:56 -0300

    INT: upddate

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -7,14 +7,9 @@ class Restaurante < ActiveRecord::Base
 	validates_presence_of :nome, message: "campo nome deve ser preenchido"
 	validates_presence_of :endereco, message: "campo endereço deve ser preenchido"
 	validates_presence_of :especialidade, message: "campos especialidade deve ser preenchido"
-
-	validades_uniqueness_of :nome, message: "nome já cadastrado"
-    
-
-  
 	validade :primeira_letra_deve_ser_maiuscula
 
-	private
+  private
 	def primeira_letra_deve_ser_maiuscula
 		errors.add(:nome,
 			"primeira letra deve ser maiúscula") unless nome =~ /[A-Z].*/
