dcd309e526abc8cbe34e5c86fa1738c79ca50890
Rafael Kerr <rafael.kerr@gmail.com>
2014-10-03 22:17:45 -0300

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+
+  VotaPrato::Application.routes.draw do
+    match 'ola'  =>  'ola_mundo#index', via: 'get'
+  end
+  
+  
+  
+  
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
